f6a4e4e3314dae863e9fd25824f651c0957ebdc4
sooin01 <sooin01@gmail.com>
2018-11-23 08:58:00 +0900

    add

--- src/main/java/com/my/app/simple/job/SimpleJob.java
+++ src/main/java/com/my/app/simple/job/SimpleJob.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.quartz.QuartzJobBean;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 public class SimpleJob extends QuartzJobBean {
 
@@ -17,16 +16,18 @@ public class SimpleJob extends QuartzJobBean {
 	private ApplicationContext applicationContext;
 
 	public SimpleJob() {
-		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+		// SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 	}
 
 	@Override
 	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
 		LOG.debug("Simple job execute start. " + applicationContext.getDisplayName());
+
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 		}
+
 		LOG.debug("Simple job execute end.");
 	}
 
