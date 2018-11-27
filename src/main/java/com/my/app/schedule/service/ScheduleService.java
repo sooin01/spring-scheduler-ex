361d9c67735c95558163b021fdd9f1e1a2d52931
sooin01 <sooin01@gmail.com>
2018-11-28 01:25:46 +0900

    add

--- src/main/java/com/my/app/schedule/service/ScheduleService.java
+++ src/main/java/com/my/app/schedule/service/ScheduleService.java
@@ -1,12 +1,23 @@
 package com.my.app.schedule.service;
 
+import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
-
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.JobKey;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.TriggerKey;
 import org.quartz.impl.StdScheduler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
@@ -15,15 +26,29 @@ import org.springframework.stereotype.Service;
 import com.my.app.simple.job.SimpleJob;
 
 @Service
-public class ScheduleService {
+public class ScheduleService implements ApplicationListener<ContextRefreshedEvent> {
+
+	private final static Logger LOD = LoggerFactory.getLogger(ScheduleService.class);
 
 	@Autowired
 	private SchedulerFactoryBean schedulerFactoryBean;
 
-	@PostConstruct
-	public void init() throws Exception {
-		StdScheduler scheduler = (StdScheduler) schedulerFactoryBean.getObject();
+	private StdScheduler scheduler;
 
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		this.scheduler = (StdScheduler) schedulerFactoryBean.getObject();
+
+		// init1();
+
+		try {
+			init2();
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void init1() throws ParseException, SchedulerException {
 		JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
 		jobDetailFactoryBean.setJobClass(SimpleJob.class);
 		jobDetailFactoryBean.setBeanName("simpleJob.class");
@@ -39,6 +64,27 @@ public class ScheduleService {
 		cronTriggerFactoryBean.afterPropertiesSet();
 
 		scheduler.scheduleJob(jobDetailFactoryBean.getObject(), cronTriggerFactoryBean.getObject());
+	}
+
+	private void init2() throws SchedulerException {
+		JobKey jobKey = new JobKey("simpleJob");
+//		scheduler.deleteJob(jobKey);
+
+		TriggerKey triggerKey = new TriggerKey("simpleJob");
+		JobDetail jobDetail = scheduler.getJobDetail(jobKey);
+		if (jobDetail == null) {
+			jobDetail = JobBuilder.newJob(SimpleJob.class).withIdentity(jobKey).build();
+		}
+		Trigger trigger = scheduler.getTrigger(triggerKey);
+		if (trigger == null) {
+			trigger = TriggerBuilder.newTrigger().forJob(jobDetail).withIdentity(triggerKey)
+					.withSchedule(CronScheduleBuilder.cronSchedule("0/3 * * * * ?")).build();
+			scheduler.scheduleJob(jobDetail, trigger);
+		}
+	}
+
+	public void process() {
+		LOD.info("Service process.");
 	}
 
 }
--- src/main/java/com/my/app/simple/job/SimpleJob.java
+++ src/main/java/com/my/app/simple/job/SimpleJob.java
@@ -5,28 +5,22 @@ import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import com.my.app.schedule.service.ScheduleService;
 
 public class SimpleJob extends QuartzJobBean {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SimpleJob.class);
 
 	@Autowired
-	private ApplicationContext applicationContext;
-
-	public SimpleJob() {
-		// SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
-	}
+	private ScheduleService scheduleService;
 
 	@Override
 	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-		LOG.debug("Simple job execute start. " + applicationContext.getDisplayName());
+		LOG.debug("Simple job execute start.");
 
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-		}
+		scheduleService.process();
 
 		LOG.debug("Simple job execute end.");
 	}
